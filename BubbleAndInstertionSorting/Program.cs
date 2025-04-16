2801391ece4720551ea29d582d4cc33808f0b282
kamilali01 <kamilaliyev20030117@gmail.com>
2025-04-16 20:47:23 +0400

    Linear and Binary Search added

--- BubbleAndInstertionSorting/Program.cs
+++ BubbleAndInstertionSorting/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
 
 public class HelloWorld
 {
@@ -8,14 +11,24 @@ public class HelloWorld
         int[] array0 = { 7, 3, 10, 5, 2 };
         Console.WriteLine("Original array:");
         PrintArray(array);
+        Console.WriteLine();
+
+        Console.WriteLine("Linear Search:");
+        LinearSearch(array, 5);
+        Console.WriteLine();
 
         BubbleSort(array);
         Console.WriteLine("Bubble Sorted array:");
         PrintArray(array);
+        Console.WriteLine();
 
         InsertionSort(array0);
         Console.WriteLine("Insertion Sorted array:");
         PrintArray(array0);
+        Console.WriteLine();
+
+        Console.WriteLine("Binary Search:");
+        BinarySearch(array, 5);
 
     }
 
@@ -31,7 +44,6 @@ public class HelloWorld
                 array[j + 1] = array[j];
                 j--;
             }
-
             array[j + 1] = current;
         }
     }
@@ -57,6 +69,44 @@ public class HelloWorld
                 return;
             }
         }
+    }
+
+    static void LinearSearch(int[] array, int target)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == target)
+            {
+                Console.WriteLine($" {target} found at {i}");
+            }
+        }
+    }
+
+    static void BinarySearch(int[] array, int target)
+    {
+        int right = array.Length - 1;
+        int left = 0;
+        
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (target == array[mid])
+            {
+                Console.WriteLine($"{target} found at {mid}");
+                return;
+            }
+            if (target > array[mid])
+            {
+                left = mid + 1;
+            }
+            if (target < array[mid])
+            {
+                right = mid - 1;
+            }
+        }
+        Console.WriteLine("Target not found");
+        return;
     }
 
     static void PrintArray(int[] array)
